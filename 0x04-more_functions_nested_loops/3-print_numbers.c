1bede4e7765f339ebd6656ab2967ac725a2c01a7
mrsalahhashim <mr.salahhashim@gmail.com>
2023-09-14 06:40:03 -0700

    c loops and more commit 13

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,18 @@
 #include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
 void print_numbers(void)
 {
-int x;
-for (x=0; x <= 9; x++)
-{
-_putchar(x + '0');
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
-_putchar('\n');
+
